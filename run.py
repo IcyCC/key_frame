45a424efec1b41a9bb7f5773bcc9e6ed0d2daad0
suchang <1103232282@qq.com>
2017-10-03 22:03:22 +0800

    upl

--- run.py
+++ run.py
@@ -5,5 +5,5 @@ if __name__ == '__main__':
     'REQUEST_TIMEOUT':10000
     }
     app.config.update(setting)
-    app.run(host="127.0.0.1", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
 
